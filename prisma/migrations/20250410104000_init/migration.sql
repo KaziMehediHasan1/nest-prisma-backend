c41366cacc0b463f095ab07f52ad1413ea5f2bb6
jayedbinnazir <jayed.freelance@gmail.com>
2025-04-10 16:42:34 +0600

    prisma models are designed

---
+++ prisma/migrations/20250410104000_init/migration.sql
@@ -0,0 +1,375 @@
+-- CreateEnum
+CREATE TYPE "FileType" AS ENUM ('IMAGE', 'VIDEO', 'AUDIO', 'DOCUMENT');
+
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'PLANNER', 'SERVICE_PROVIDER', 'VENUE_OWNER', 'GUEST');
+
+-- CreateEnum
+CREATE TYPE "ServiceProviderRole" AS ENUM ('PHOTOGRAPHER', 'VIDEOGRAPHER', 'DJ_BAND', 'CATERING', 'ENTERTAINER');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('PENDING', 'COMPLETED', 'FAILED', 'REFUNDED', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "PaymentMethod" AS ENUM ('CREDIT_CARD', 'DEBIT_CARD', 'PAYPAL', 'BANK_TRANSFER', 'CASH');
+
+-- CreateEnum
+CREATE TYPE "BookingStatus" AS ENUM ('REQUESTED', 'PENDING', 'CONFIRMED', 'COMPLETED');
+
+-- CreateEnum
+CREATE TYPE "AcceptanceStatus" AS ENUM ('ACCEPTED', 'DENIED');
+
+-- CreateEnum
+CREATE TYPE "TableShape" AS ENUM ('ROUND', 'OVAL', 'HALF_MOON', 'BANQUET', 'SQUARE', 'HEXAGONAL');
+
+-- CreateEnum
+CREATE TYPE "SeatingStyle" AS ENUM ('BANQUET', 'THEATER', 'CLASSROOM', 'CABARET', 'U_SHAPED', 'COCKTAIL');
+
+-- CreateEnum
+CREATE TYPE "LightingStyle" AS ENUM ('AMBIENT', 'SPOTLIGHTING', 'FAIRY', 'CHANDELIERS', 'LED', 'NEON', 'GOBO');
+
+-- CreateEnum
+CREATE TYPE "FlowerColor" AS ENUM ('WHITE', 'RED', 'YELLOW', 'PINK', 'PURPLE', 'BLUE', 'GREEN', 'ORANGE');
+
+-- CreateEnum
+CREATE TYPE "FlowerType" AS ENUM ('ROSES', 'PEONIES', 'LILIES', 'ORCHIDS', 'TULIPS', 'SUNFLOWERS', 'HYDRANGEAS');
+
+-- CreateEnum
+CREATE TYPE "Fragrance" AS ENUM ('FLORAL_SCENTS', 'CITRUS_SCENTS', 'HERBAL_SCENTS', 'OCEANIC_SCENTS', 'WOODY_SCENTS', 'SPICY');
+
+-- CreateEnum
+CREATE TYPE "BookingType" AS ENUM ('INSTANT_BOOKING', 'REQUEST_BASED_BOOKING');
+
+-- CreateEnum
+CREATE TYPE "VenueType" AS ENUM ('HOTEL', 'RESTAURANT', 'CONFERENCE_HALL', 'BANQUET', 'RESORT', 'OUTDOOR');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'PLANNER',
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "imageId" TEXT,
+    "location" TEXT,
+    "coverPhotoId" TEXT,
+    "profession" TEXT,
+    "description" TEXT,
+    "experience" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "EventType" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "EventType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Venue" (
+    "id" TEXT NOT NULL,
+    "profileId" TEXT,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "area" TEXT NOT NULL,
+    "description" TEXT,
+    "capacity" INTEGER NOT NULL,
+    "bookedDates" TIMESTAMP(3)[],
+    "type" "VenueType" NOT NULL,
+    "cateringDescription" TEXT,
+    "parkingDescription" TEXT,
+    "availabilityDescription" TEXT,
+    "extraServiceDescription" TEXT,
+    "price" INTEGER NOT NULL,
+    "bookingType" "BookingType" NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Venue_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" TEXT NOT NULL,
+    "venueId" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Shift" (
+    "id" TEXT NOT NULL,
+    "venueId" TEXT NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "shiftName" TEXT NOT NULL,
+    "employeeId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Shift_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" TEXT NOT NULL,
+    "payerId" TEXT NOT NULL,
+    "receiverId" TEXT NOT NULL,
+    "paymentStatus" "PaymentStatus" NOT NULL,
+    "paymentMethod" "PaymentMethod" NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "transactionId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" TEXT NOT NULL,
+    "bookedById" TEXT NOT NULL,
+    "venueId" TEXT NOT NULL,
+    "venueName" TEXT NOT NULL,
+    "serviceProviderId" TEXT NOT NULL,
+    "eventName" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "plannerName" TEXT NOT NULL,
+    "selectedDate" TIMESTAMP(3) NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "eventType" TEXT NOT NULL,
+    "bookingType" "BookingType" NOT NULL,
+    "guestNumber" INTEGER NOT NULL,
+    "decoration" TEXT,
+    "services" TEXT[],
+    "totalCost" INTEGER NOT NULL,
+    "bookingStatus" "BookingStatus" NOT NULL,
+    "paid" INTEGER NOT NULL,
+    "due" INTEGER NOT NULL,
+    "paymentId" TEXT,
+    "paymentMethod" "PaymentMethod" NOT NULL,
+    "accept" "AcceptanceStatus" NOT NULL,
+    "isEventFinished" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Decoration" (
+    "id" TEXT NOT NULL,
+    "venueId" TEXT NOT NULL,
+    "tableShapes" "TableShape"[],
+    "seatingStyles" "SeatingStyle"[],
+    "lighting" "LightingStyle"[],
+    "flowerColors" "FlowerColor"[],
+    "flowerTypes" "FlowerType"[],
+    "fragrances" "Fragrance"[],
+
+    CONSTRAINT "Decoration_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" TEXT NOT NULL,
+    "venueId" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "profileId" TEXT,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Amenities" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "default" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Amenities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FileInstance" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "fileId" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "bucket" TEXT NOT NULL,
+    "type" "FileType" NOT NULL,
+    "venueId" TEXT,
+    "eventPreferenceId" TEXT,
+    "profileId" TEXT,
+
+    CONSTRAINT "FileInstance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_EventTypeToProfile" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_EventTypeToProfile_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_AmenitiesToVenue" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_AmenitiesToVenue_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_imageId_key" ON "Profile"("imageId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_coverPhotoId_key" ON "Profile"("coverPhotoId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "EventType_name_key" ON "EventType"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_email_key" ON "Employee"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_phone_key" ON "Employee"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Booking_paymentId_key" ON "Booking"("paymentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Decoration_venueId_key" ON "Decoration"("venueId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Amenities_name_key" ON "Amenities"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FileInstance_fileId_key" ON "FileInstance"("fileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FileInstance_venueId_key" ON "FileInstance"("venueId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FileInstance_eventPreferenceId_key" ON "FileInstance"("eventPreferenceId");
+
+-- CreateIndex
+CREATE INDEX "_EventTypeToProfile_B_index" ON "_EventTypeToProfile"("B");
+
+-- CreateIndex
+CREATE INDEX "_AmenitiesToVenue_B_index" ON "_AmenitiesToVenue"("B");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_imageId_fkey" FOREIGN KEY ("imageId") REFERENCES "FileInstance"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_coverPhotoId_fkey" FOREIGN KEY ("coverPhotoId") REFERENCES "FileInstance"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Venue" ADD CONSTRAINT "Venue_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES "Venue"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Shift" ADD CONSTRAINT "Shift_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES "Venue"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Shift" ADD CONSTRAINT "Shift_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_payerId_fkey" FOREIGN KEY ("payerId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_bookedById_fkey" FOREIGN KEY ("bookedById") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES "Venue"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_serviceProviderId_fkey" FOREIGN KEY ("serviceProviderId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_paymentId_fkey" FOREIGN KEY ("paymentId") REFERENCES "Payment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Decoration" ADD CONSTRAINT "Decoration_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES "Venue"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES "Venue"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FileInstance" ADD CONSTRAINT "FileInstance_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES "Venue"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FileInstance" ADD CONSTRAINT "FileInstance_eventPreferenceId_fkey" FOREIGN KEY ("eventPreferenceId") REFERENCES "EventType"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FileInstance" ADD CONSTRAINT "FileInstance_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_EventTypeToProfile" ADD CONSTRAINT "_EventTypeToProfile_A_fkey" FOREIGN KEY ("A") REFERENCES "EventType"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_EventTypeToProfile" ADD CONSTRAINT "_EventTypeToProfile_B_fkey" FOREIGN KEY ("B") REFERENCES "Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_AmenitiesToVenue" ADD CONSTRAINT "_AmenitiesToVenue_A_fkey" FOREIGN KEY ("A") REFERENCES "Amenities"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_AmenitiesToVenue" ADD CONSTRAINT "_AmenitiesToVenue_B_fkey" FOREIGN KEY ("B") REFERENCES "Venue"("id") ON DELETE CASCADE ON UPDATE CASCADE;
