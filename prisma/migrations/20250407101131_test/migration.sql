34f233dab4483b379f2bf089811a4feb3d9ef907
arafat2020 <arafatmannan2019@gmail.com>
2025-04-16 09:46:21 +0600

    tye

--- prisma/migrations/20250407101131_test/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Venue" ADD COLUMN     "availability" TEXT,
-ADD COLUMN     "catering" TEXT,
-ADD COLUMN     "extraServices" TEXT,
-ADD COLUMN     "parking" TEXT;
--- prisma/migrations/20250409082522_test/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ServicePRoviderRole" AS ENUM ('PHOTOGRAPHER', 'VIDEOGRAPHER', 'DJ_BAND', 'catering');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "servicePRoviderRole" "ServicePRoviderRole";
