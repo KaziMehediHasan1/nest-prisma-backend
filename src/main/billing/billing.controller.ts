b0d95a33537a76e9f97d9af8576ba240d58db871
arafat2020 <arafatmannan2019@gmail.com>
2025-04-18 17:45:51 +0600

    minor fix

--- src/main/billing/billing.controller.ts
+++ src/main/billing/billing.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { BillingService } from './billing.service';
-import { CreatePaymentIntentDto } from './dto/createPayment.dto';
+import { CreatePaymentIntentDtoWithId } from './dto/createPayment.dto';
+import Stripe from 'stripe';
 
 @Controller('billing')
 export class BillingController {
     constructor(private readonly billingService: BillingService) {}
 
     @Post('create-payment-session')
-    async createPaymentSession(@Body() payment: CreatePaymentIntentDto) {
-        return this.billingService.createPaymentSession(payment);
+    async createPaymentSession(@Body() payment: CreatePaymentIntentDtoWithId) {
+        return this.billingService.createCheckoutSession(payment);
+    }
+
+    @Post('webhook')
+    async handleCheckoutSessionCompleted(@Body() session: Stripe.Event) {
+        this.billingService.handleStripeEvent(session);
     }
 }
