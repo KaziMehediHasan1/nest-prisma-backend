9e3dce6ccdcee8462c98e56b05d6164696fd4be6
arafat2020 <arafatmannan2019@gmail.com>
2025-05-12 11:58:11 +0600

    minor fix

--- src/main/auth/profile/dto/updateProfile.dto.ts
+++ src/main/auth/profile/dto/updateProfile.dto.ts
@@ -1,6 +1,28 @@
-import { PartialType } from "@nestjs/swagger";
+import { ApiProperty, PartialType } from "@nestjs/swagger";
 import { SetupPlannerProfileDto, SetupServiceProviderProfileDto, SetupVenueOwnerProfileDto } from "./setupProflie.dto";
+import { IsString, MaxLength } from "class-validator";
 
-export class UpdatePlannerProfile extends PartialType(SetupPlannerProfileDto) {}
-export class UpdateServiceProviderProfile extends PartialType(SetupServiceProviderProfileDto) {}
-export class UpdateVenueOwnerProfile extends PartialType(SetupVenueOwnerProfileDto) {}
+export class UpdatePlannerProfile extends PartialType(SetupPlannerProfileDto) {
+     @ApiProperty({
+        description: 'User address',
+        example: 'Jayed Bin Nazir 253',
+      })
+      @IsString({ message: 'Username must be a string' })
+      userName: string;
+}
+export class UpdateServiceProviderProfile extends PartialType(SetupServiceProviderProfileDto) {
+    @ApiProperty({
+        description: 'User address',
+        example: 'Jayed Bin Nazir 253',
+      })
+      @IsString({ message: 'Username must be a string' })
+      userName: string;
+}
+export class UpdateVenueOwnerProfile extends PartialType(SetupVenueOwnerProfileDto) {
+       @ApiProperty({
+        description: 'User address',
+        example: 'Jayed Bin Nazir 253',
+      })
+      @IsString({ message: 'Username must be a string' })
+      userName: string;
+}
