e3edde347f0a78e69b6f9e5c67a3c69febd213d8
md-sakib-softvence <sakibsoftvence@gmail.com>
2025-05-16 14:39:23 +0600

    fix: guset notification payment   module add services mudle

--- src/main/guest/guest.module.ts
+++ src/main/guest/guest.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { GuestService } from './guest.service';
+import { GuestService } from './services/guest.service';
 import { GuestController } from './guest.controller';
 import { AuthService } from '../auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { GuestLinkService } from './services/guestLink.service';
 
 @Module({
   controllers: [GuestController],
-  providers: [GuestService, AuthService, JwtService],
+  providers: [GuestService, AuthService, JwtService, GuestLinkService],
 })
 export class GuestModule {}
