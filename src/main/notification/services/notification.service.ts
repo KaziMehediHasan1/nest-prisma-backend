19aca8d64ba67af21549a85fb712a2effd9a0acc
arafat2020 <arafatmannan2019@gmail.com>
2025-05-19 12:39:46 +0600

    minor fix

--- src/main/notification/services/notification.service.ts
+++ src/main/notification/services/notification.service.ts
@@ -1,4 +1,78 @@
 import { Injectable } from '@nestjs/common';
+import { FirebaseService } from './firebase.service';
+import { SendNotificationDto } from '../dto/sendNotification.dto';
+import { DbService } from 'src/lib/db/db.service';
+import { IdDto } from 'src/common/dto/id.dto';
+import { SaveFcmTokenDto } from '../dto/saveFcm.dot';
+import { ApiResponse } from 'src/interfaces/response';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+    constructor(
+        private readonly firebaseService: FirebaseService,
+        private readonly db: DbService
+    ) {}
+
+    async sendPushNotification(
+   { token, title, body, data }: SendNotificationDto,
+  ) {
+    try {
+      const message = {
+        token,
+        notification: {
+          title,
+          body,
+        },
+        data: data || {},
+      };
+
+      const response = await this.firebaseService.getMessaging().send(message);
+      return { success: true, response };
+    } catch (error) {
+      return { success: false, error };
+    }
+  }
+
+  public async saveFcm({id}:IdDto,{token}:SaveFcmTokenDto):Promise<ApiResponse<any>>{
+    const isExist = await this.db.fcmToken.findUnique({
+    where:{
+        profileId:id
+    }
+    });
+    if (isExist) {
+        const data = await this.db.fcmToken.update({
+            where:{
+                profileId:id
+            },
+            data:{
+                token
+            }
+        })
+
+        return {
+            data,
+            message: 'FCM token updated successfully',
+            statusCode: 200,
+            success: true,
+        };
+    }
+    
+    const created = await this.db.fcmToken.create({
+        data:{
+            token,
+            Profile:{
+                connect:{
+                    id
+                }
+            }
+        }
+    })
+    return {
+        data: created,
+        message: 'FCM token created successfully',
+        statusCode: 200,
+        success: true,
+    };
+
+  }
+}
